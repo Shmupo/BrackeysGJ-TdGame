26e561d06957ba5f184dd5bb0a2194fc0ff52c97
Shmupo <andrewdoan8b@gmail.com>
2024-09-09 20:34:46 -0700

    Add ModItems, Mod Slots

---
+++ Scripts/ModItem/DragComponent.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+@onready var modItem: ModItem = $".."
+@onready var dragButton: Button = $"../DragButton"
+
+var dragging: bool = false
+var offset: Vector2 = Vector2(0, 0)
+# how far you can drop this from a mod slot to snap it in
+var snapDistance: float = 50
+var modSlot: ModSlot = null
+
+
+func _ready() -> void:
+	dragButton.button_down.connect(onDrag)
+	dragButton.button_up.connect(onReleaseFromDrag)
+	
+	
+func onDrag() -> void:
+	dragging = true
+	offset = get_global_mouse_position() - modItem.global_position
+
+
+# try to snap to a modSlot if close enough
+func onReleaseFromDrag() -> void:
+	if modSlot:
+		modSlot.removeModItemFromSlot()
+	dragging = false
+	snapToSlotInRange()
+
+
+# is the user drops this items within snapDistance of a "ModSlot" group item, snap to it
+func snapToSlotInRange() -> void:
+	var modSlots: Array[Node] = get_tree().get_nodes_in_group("ModSlot")
+	
+	for slot: ModSlot in modSlots:
+		if modItem.global_position.distance_to(slot.global_position) < snapDistance:
+			slot.placeModItemInSlot(modItem)
+			modSlot = slot
+
+
+func _process(delta: float) -> void:
+	if dragging:
+		modItem.global_position = get_global_mouse_position() - offset
---
+++ Scripts/ModMenus/ModSlot.gd
@@ -0,0 +1,42 @@
+class_name ModSlot
+extends TextureRect
+
+signal slotAdd(modItem: ModItem)
+signal slotRemove(modItem: ModItem)
+
+var modItem: ModItem = null
+# This is a node2D placed at the center of this button - it has no other script or function
+@onready var modSlotCenterNode: Node2D = $ModSlotCenter
+
+
+func _ready() -> void:
+	add_to_group("ModSlot")
+
+
+# use this to place and move a modItem into this slot
+func placeModItemInSlot(newModItem: ModItem) -> void:
+	if modItem == null:
+		modItem = newModItem
+		newModItem.global_position = modSlotCenterNode.global_position
+		slotAdd.emit(modItem)
+
+
+# adds item to a slot without any signaling
+# used by TowerMenu to load a ModItem without triggering the signal to add the mod to the tower again, causing a dupwication gwitch
+func placeModItemInSlotNoSignal(newModItem: ModItem) -> void:
+	if modItem == null:
+		modItem = newModItem
+		newModItem.global_position = modSlotCenterNode.global_position
+
+
+# clears slot, and if connected to TowerMenu, signals for mod removal from tower
+func removeModItemFromSlot() -> void:
+	if modItem != null:
+		slotRemove.emit(modItem)
+		modItem = null
+
+
+# used to clear this slot, but not remove the mod from the tower (if if used in TowerMenu)
+func removeModItemNoSignal() -> void:
+	if modItem != null:
+		modItem = null
