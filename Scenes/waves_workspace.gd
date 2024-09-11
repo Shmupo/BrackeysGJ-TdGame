cb39ac2ad4c9f6d3ddae6b114805fce70086d082
Bacta Tanks <bactatanks9@gmail.com>
2024-09-10 18:00:53 -0700

    waves workspace scene barely working

--- Scenes/waves_workspace.gd
+++ Scenes/waves_workspace.gd
@@ -5,18 +5,22 @@ extends Node2D
 @onready var tile_map_layer: TileMapLayer = $UserInteractableTileMapLayer
 @onready var input_component: InputComponent = $UserInteractableTileMapLayer/InputComponent
 
-# Called when the node enters the scene tree for the first time.
+func do_nothing() -> void:
+	pass
+
+
 func _ready() -> void:
-	input_component.queue_free()
+	# Create path based tile map
 	var map = tile_map_layer.tile_map_data
 	for i in 100:
 		for j in 100:
 			var cell = Vector2i(-1 * j, -1 * i)
 			var result = tile_map_layer.get_cell_atlas_coords(cell)
-			if result.x == 3 and result.y == 0:
-				tile_map_layer.set_cell(cell, 0, Vector2i(7, 0))
-				pathing_component.placePath(cell)
+			if result.x == 4 and result.y == 0:
+				pathing_component.aStarGrid.set_point_solid(cell, false)
 	pathing_component.updatePath()
+	
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
