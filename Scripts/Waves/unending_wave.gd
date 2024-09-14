ee78f9a9e881667c31ddbd7c11704efdeddc1e18
bactatanks <bactatanks9@gmail.com>
2024-09-14 12:15:54 -0700

    Improved unending wave

--- Scripts/Waves/unending_wave.gd
+++ Scripts/Waves/unending_wave.gd
@@ -2,7 +2,7 @@ extends DictWave
 
 @onready var rng = RandomNumberGenerator.new()
 
-@export var num_entities_2: int = 10  # hacks
+var num_entities_2: int = 10  # hacks.  ignore here.
 @export var entity_speed: int = 50
 @export var entity_health: int = 5
 @export var entity_points = 30
@@ -11,23 +11,27 @@ extends DictWave
 @export var spawn_delay = 2
 
 
+@export var entities_before_increase: int = 10
+var difficulty = 1
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spawn_queue = []
 	
-	for i in range(num_entities_2):
+	for i in range(entities_before_increase + 5):
 		spawn_queue.append(create_entity_dict())
+
 
 func _process(delta: float) -> void:
 	if spawn_queue.size() < 5:
 		increase_difficulty()
-		for i in 20:
-			spawn_queue.append(gen_entity)
+		for i in entities_before_increase:
+			spawn_queue.append(gen_entity())
 
 
 func increase_difficulty() -> void:
-	# TODO: implement this
-	pass
+	difficulty += 1
 
 
 func gen_entity() -> Dictionary:
@@ -36,13 +40,19 @@ func gen_entity() -> Dictionary:
 
 
 func create_entity_dict() -> Dictionary:
+	var min_spawn_delay: float = 0.1
+	
+	var modified_delay: float = spawn_delay - (float(difficulty) / 10)
+	if modified_delay < min_spawn_delay:
+		modified_delay = min_spawn_delay
+	
 	return {
 		"type": entity_type,
-		"spawn_delay": spawn_delay,
+		"spawn_delay": modified_delay,
 		"config": {
-			"speed": entity_speed,
-			"health": entity_health,
-			"strength": entity_strength,
-			"points": entity_points
+			"speed": int(float(entity_speed) * (1.0 + (float(difficulty) / 15))),
+			"health": int(float(entity_health) * (1.0 + (float(difficulty) / 10))),
+			"strength": int(float(entity_strength) * (1.0 + float(difficulty) / 10)),
+			"points": int(float(entity_points) * (1.0 + float(difficulty) / 10))
 		}
 	}
